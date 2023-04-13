81a79df06a295a94dd9df158a4b5762482f56bce
betocoutinho <robertogmcfilho@gmail.com>
2023-04-13 08:35:46 -0300

    Projeto finalizado

--- app/src/main/java/com/example/anamnesev1/AnamnaseProcess.java
+++ app/src/main/java/com/example/anamnesev1/AnamnaseProcess.java
@@ -3,27 +3,22 @@ package com.example.anamnesev1;
 import java.util.ArrayList;
 
 public class AnamnaseProcess {
-    private int idade;
     private ArrayList<Boolean> case1 = new ArrayList<Boolean>();
     private ArrayList<Boolean> case2 = new ArrayList<Boolean>();
-
-    public AnamnaseProcess(int idade){
-        this.idade = idade;
-    }
 
     public void feedCase1(boolean status){
         this.case1.add(status);
     }
 
-    public void feedCase3(boolean status){
+    public void feedCase2(boolean status){
         this.case2.add(status);
     }
 
-    public String processReturn(){
+    public String processReturn(int idade){
 
-        if (case1.size() >= 7 && case1.stream().allMatch(t -> t == true) ){
-            return "Procure um cardiologista com urgencia";
-        }else if(case2.size() >= 7 && case2.stream().allMatch(t -> t == true)){
+        if (case1.size() >= 7 && case1.stream().allMatch(t -> t == true ) && idade >= 45){
+            return "Procure um Cardiologista com Urgencia";
+        }else if(case2.size() >= 5 && case2.stream().allMatch(t -> t == true) && idade < 45){
             return "O paciente não apresenta no momento nenhuma queixa\n" +
                     "que possa ser encaminhado a alguma especialidade médica";
         }else{
--- app/src/main/java/com/example/anamnesev1/MainActivity.java
+++ app/src/main/java/com/example/anamnesev1/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.anamnesev1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private AnamnaseProcess anamnese;
+    private AnamnaseProcess anamnese = new AnamnaseProcess();
     private String mensagem;
 
     private EditText idade;
@@ -24,11 +26,100 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onRadioButtonClicked(View view){
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
+        switch(view.getId()) {
+            case R.id.rbmasc:
+                if (checked)
+                    anamnese.feedCase1(true);
+                    anamnese.feedCase2(true);
+                break;
+            case R.id.rbfem:
+                if (checked)
+                    anamnese.feedCase1(true);
+                    anamnese.feedCase2(true);
+                break;
+            case R.id.rbm60:
+                if(checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.rb2040:
+                if (checked)
+                    anamnese.feedCase2(true);
+                break;
+            case R.id.radioButton13:
+                if(checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.radioButton14:
+                if(checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.radioButton25:
+                if(checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.radioButton24:
+                if (checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.radioButton10:
+                if (checked)
+                    anamnese.feedCase2(true);
+                break;
+            case R.id.radioButton11:
+                if (checked)
+                    anamnese.feedCase2(true);
+                break;
+        }
     }
 
     public void onCheckboxClicked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
 
+        switch (view.getId()){
+            case R.id.levantarpeso:
+                if (checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.cbpai:
+                if (checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.cbmae:
+                if (checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.cbcoracao:
+                if (checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.cbpressart:
+                if (checked)
+                    anamnese.feedCase1(true);
+                break;
+            case R.id.caminhar:
+                if (checked)
+                    anamnese.feedCase2(true);
+                break;
+            case R.id.cbavo:
+                if (checked)
+                    anamnese.feedCase2(true);
+                break;
+            case R.id.cbproblemasmusc:
+                if (checked)
+                    anamnese.feedCase2(true);
+                break;
+        }
+    }
+
+    public void execButton(View view){
+        int idade = Integer.parseInt(this.idade.getText().toString());
+
+        mensagem = anamnese.processReturn(idade);
+
+        result.setText(mensagem);
     }
 
 }
